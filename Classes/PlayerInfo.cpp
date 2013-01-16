a78ed6eb5e82b257c0540a6d022e8ef9b7c2ba08
ThunderSword <lightning_0721@163.com>
2013-01-17 00:23:34 +0800

    支持英雄单位文件读写，读的路径有待于改进

--- Classes/PlayerInfo.cpp
+++ Classes/PlayerInfo.cpp
@@ -3,6 +3,7 @@
 #include "Unit.h"
 #include "SkillInfo.h"
 #include "PlayerInfo.h"
+#include "GameFile.h"
 
 
 void CHeroUpdate::updateMaxExp( CLevelExp* pLevel )
@@ -56,22 +57,108 @@ void CHeroUnit::updateMaxExp()
     m_dwMaxExp *= 2;
 }
 
-CGameUnit* CHeroInfo::createHero()
+CHeroUnit* CHeroInfo::createHero()
 {
     M_DEF_UM(pUm);
-    CGameUnit* pHero = pUm->unitByInfo(m_iIndex); // will be insteaded by patch
+    CHeroUnit* pHero = pUm->heroByInfo(m_iHeroIndex); // will be insteaded by patch
     //M_DEF_UPM(pUpm);
     //CGameUnit* pHero = pUpm->unitByIndex(m_iIndex);
     pHero->m_dwLvl = m_dwLevel;
     pHero->m_dwMaxLvl = m_dwMaxLevel;
     pHero->m_dwExp = m_dwExp;
     pHero->m_dwMaxExp = m_dwMaxExp;
-    for (VEC_SKILLS::iterator it = m_vecSkill.begin(); it != m_vecSkill.end(); ++it)
+    for (VEC_SKILLS::iterator it = m_vecSkills.begin(); it != m_vecSkills.end(); ++it)
     {
         pHero->addSkill(g_oOrgSkillInfo.skill(*it));
     }
     
     return pHero;
+}
+
+uint16_t CHeroInfo::CONST_FILE_DATA_SIZE
+    = sizeof(m_iHeroIndex)
+    + sizeof(m_dwExp)
+    + sizeof(m_dwMaxExp)
+    + sizeof(m_dwLevel)
+    + sizeof(m_dwMaxLevel);
+
+bool CHeroInfo::initWithFileStream( CGameFile* pFile )
+{
+    uint16_t wDataSize = 0;
+    if (pFile->read(&wDataSize) != 1)
+    {
+        return false;
+    }
+    size_t uPos = pFile->tell();
+    bool bRet = false;
+    do
+    {
+        m_vecSkills.clear();
+        uint16_t wSkillCount;
+        if (wDataSize != CONST_FILE_DATA_SIZE
+            || pFile->read(&m_iHeroIndex) != 1
+            || pFile->read(&m_dwMaxLevel) != 1
+            || pFile->read(&m_dwLevel) != 1
+            || pFile->read(&m_dwMaxExp) != 1
+            || pFile->read(&m_dwExp) != 1
+            || pFile->read(&wSkillCount) != 1)
+        {
+            break;
+        }
+        bool bBreak = false;
+        for (uint16_t i = 0; i < wSkillCount; ++i)
+        {
+            int iSkill;
+            if (pFile->read(&iSkill) != 1)
+            {
+                bBreak = true;
+                break;
+            }
+            m_vecSkills.push_back(iSkill);
+        }
+        if (bBreak)
+        {
+            break;
+        }
+
+        bRet = true;
+
+    } while (false);
+
+    if (!bRet)
+    {
+        pFile->seek(uPos, CGameFile::kBegin);
+    }
+    return bRet;
+}
+
+void CHeroInfo::writeToFileStream( FILE* pFile )
+{
+    fwrite(&CONST_FILE_DATA_SIZE, sizeof(CONST_FILE_DATA_SIZE), 1, pFile);
+    fwrite(&m_iHeroIndex, sizeof(m_iHeroIndex), 1, pFile);
+    fwrite(&m_dwMaxLevel, sizeof(m_dwMaxLevel), 1, pFile);
+    fwrite(&m_dwLevel, sizeof(m_dwLevel), 1, pFile);
+    fwrite(&m_dwMaxExp, sizeof(m_dwMaxExp), 1, pFile);
+    fwrite(&m_dwExp, sizeof(m_dwExp), 1, pFile);
+    uint16_t wSkillCount = m_vecSkills.size();
+    fwrite(&wSkillCount, sizeof(wSkillCount), 1, pFile);
+    for (uint16_t i = 0; i < wSkillCount; ++i)
+    {
+        fwrite(&m_vecSkills[i], sizeof(m_vecSkills[i]), 1, pFile);
+    }
+}
+
+void CHeroInfo::updateHeroLevel( CHeroUnit* pUnit )
+{
+    m_dwMaxLevel = pUnit->getMaxLevel();
+    m_dwLevel = pUnit->getLevel();
+    m_dwMaxExp = pUnit->getMaxExp();
+    m_dwExp = pUnit->getExp();
+}
+
+void CHeroInfo::addSkill( int iOrgSkillIndex )
+{
+    m_vecSkills.push_back(iOrgSkillIndex);
 }
 
 CPlayerInfo::CPlayerInfo(void)
@@ -81,3 +168,4 @@ CPlayerInfo::CPlayerInfo(void)
 CPlayerInfo::~CPlayerInfo(void)
 {
 }
+
--- Classes/PlayerInfo.h
+++ Classes/PlayerInfo.h
@@ -19,23 +19,32 @@ public:
 
     virtual void onLevelChange(int32_t iChanged);
     virtual void updateMaxExp();
+
+public:
+    //VEC_SKILLS m_vecSkill;
 };
 
 class CHeroInfo
 {
 public:
     typedef vector<int> VEC_SKILLS;
+    static uint16_t CONST_FILE_DATA_SIZE;
 
 public:
-    CGameUnit* createHero();
+    virtual bool initWithFileStream(CGameFile* pFile);
+    virtual void writeToFileStream(FILE* pFile);
+    CHeroUnit* createHero();
+    void updateHeroLevel(CHeroUnit* pUnit);
+    void addSkill(int iOrgSkillIndex);
 
 public:
-    int m_iIndex;
-    VEC_SKILLS m_vecSkill;
-    uint32_t m_dwLevel;
+    int m_iHeroIndex;
+    VEC_SKILLS m_vecSkills; // org skill index
     uint32_t m_dwMaxLevel;
-    uint32_t m_dwExp;
+    uint32_t m_dwLevel;
     uint32_t m_dwMaxExp;
+    uint32_t m_dwExp;
+    
 };
 
 class CPlayerInfo
@@ -43,6 +52,8 @@ class CPlayerInfo
 public:
     CPlayerInfo(void);
     virtual ~CPlayerInfo(void);
+
+    //virtual bool initWithFile();
 
 public:
     vector<CHeroInfo> m_vecHero;
