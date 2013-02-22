6b6afd0c8bbb6328e9ca27e06a35403f1c636ef4
wangbr <wangbr_numb@163.com>
2013-02-22 23:20:31 +0800

    ?

---
+++ proj.win32/damageTest.h
@@ -0,0 +1,20 @@
+class  VariableTest
+{
+public:
+    VariableTest();
+public:
+    int heroBattleResult()
+    {
+        int hpLeft=heroDamage;
+        return hpLeft;
+    }
+public:
+    int enemyDamage;
+    int enemyNumber;
+    int enemyHp;
+
+    int heroDamage;
+    int heroHp;
+    int heroHpIncrease;
+
+};
