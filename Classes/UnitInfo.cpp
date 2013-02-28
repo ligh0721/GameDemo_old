d74ca60fc18a358dc3a0f94f629d0ad752680442
ThunderSword <lightning_0721@163.com>
2013-02-28 19:08:57 +0800

    优化火柴人挥剑，旋转剑刃动作； 新增火柴人抡大锤动作

--- Classes/UnitInfo.cpp
+++ Classes/UnitInfo.cpp
@@ -81,6 +81,7 @@ bool COrgUnitInfo::init()
     pGm->loadUnitAnimation("Matchstick", "act2");
     pGm->loadUnitAnimation("Matchstick", "act3");
     pGm->loadUnitAnimation("Matchstick", "act4");
+    pGm->loadUnitAnimation("Matchstick", "act5");
 
     pGm->loadUnitAnimation("Kurokisi", "move");
     pGm->loadUnitAnimation("Kurokisi", "die");
@@ -307,8 +308,8 @@ bool COrgUnitInfo::init()
         ccp(59.0 / 128, 6.0 / 128),    // archor point
         24, 27,    // 0.5w 0.5h
         1,
-        0.1, 0.1, 0.08, 0.08, 0.1, 0.05, 0.00, 0.00,
-        CUnitInfo::ARR_ATTACK_ANI(3, kAct1, kAct2, kAct4),
+        0.1, 0.1, 0.08, 0.08, 0.1, 0.05, 0.1, 0.00,
+        CUnitInfo::ARR_ATTACK_ANI(4, kAct1, kAct2, kAct4, kAct5),
         40,
         2, 0.25, 0, 34, 150, CGameUnit::kWTClosely,
         0, 0, 0, 0, 41, 29,
